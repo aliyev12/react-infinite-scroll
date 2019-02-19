d739795bbbe517d2179380fbbd6cfb3856d5a84f
Jelly Bean <aaa7c4@gmail.com>
2019-02-19 09:25:49 -0500

    first commit

---
+++ config/server.js
@@ -0,0 +1,7 @@
+const env = require('dotenv').config();
+
+module.exports = {
+    APPLICATION_ID: process.env.APPLICATION_ID,
+    SECRET: process.env.SECRET,
+    CALLBACK_URL: process.env.CALLBACK_URL
+}
